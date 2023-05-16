46d0bf7c1be61c6021e737fb31193d3c83499674
Joy A <itsjoy@gmail.com>
2023-05-16 17:20:29 +0530

    avniproject/avni-webapp#967 | Fix DashboardFilterConfigTest

--- test/reports/DashboardFilterConfigTest.js
+++ test/reports/DashboardFilterConfigTest.js
@@ -6,7 +6,6 @@ import TestConceptFactory from "../ref/TestConceptFactory";
 describe('DashboardFilterConfigTest', () => {
   it('is valid', () => {
     const dashboardFilterConfig = new DashboardFilterConfig();
-    dashboardFilterConfig.setSubjectType(SubjectTypeFactory.create({uuid: "ST1"}));
     dashboardFilterConfig.setType(CustomFilter.type.Concept);
     dashboardFilterConfig.observationBasedFilter.concept = TestConceptFactory.create({uuid: "C1"});
     dashboardFilterConfig.observationBasedFilter.setScope(CustomFilter.scope.Registration);
