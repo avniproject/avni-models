c582ad6261feaeeb5e1a069a53804196db9b2a66
Maha Lakshme S <mahalakshme1994@gmail.com>
2023-06-26 19:13:27 +0530

    avniproject/avni-client#1035 | Add method to get two level addresses on Individual

--- src/Individual.js
+++ src/Individual.js
@@ -955,6 +955,13 @@ class Individual extends BaseEntity {
     return this.isPerson() ? this.getDisplayAge(i18n) : "";
   }
 
+  subjectAddressText(i18n) {
+    const parentAddress = _.get(this, 'lowestAddressLevel.locationMappings[0].parent.name');
+    let addressText = i18n.t(this.lowestAddressLevel.name);
+    if(!_.isNil(parentAddress)) addressText += ', ' + i18n.t(parentAddress);
+    return addressText;
+  }
+
   //TODO these methods are slightly differece because of differece in UI on search result and my dashboard listing. Not taking the hit right now.
   detail1(i18n) {
     return this.isPerson() ? {label: "Age", value: this.getDisplayAge(i18n)} : {};
