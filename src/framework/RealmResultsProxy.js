dff3adca6505eefdd8aec196b78eb287f53289bb
Vivek Singh <petmongrels@gmail.com>
2023-12-07 11:46:50 +0530

    #000 pass on log queries to realmresults created by filters

--- src/framework/RealmResultsProxy.js
+++ src/framework/RealmResultsProxy.js
@@ -4,141 +4,143 @@ import _ from "lodash";
 //https://www.mongodb.com/docs/realm-sdks/js/latest/Realm.Collection.html
 //RealmCollection as per realm documentation returns RealmResults. But there are no extra methods/properties in realm results, so we are using realm collection proxy for realm results and don't have a separate proxy class for it
 class RealmResultsProxy {
-  static create(realmCollection, entityClass) {
-    return new Proxy(new RealmResultsProxy(realmCollection, entityClass), RealmResultsProxyHandler);
-  }
-
-  constructor(realmCollection, entityClass) {
-    this.entityClass = entityClass;
-    this.realmCollection = realmCollection;
-    this.array = [];
-  }
-
-  setLogQueries(value) {
-    this.logQueries = value;
-  }
-
-  createEntity(object) {
-    return new this.entityClass(object);
-  }
-
-  asArray() {
-    this.materialiseArray();
-    return this.array;
-  }
-
-  materialiseArray() {
-    if (this.array.length !== this.realmCollection.length) {
-      this.realmCollection.forEach((object, index) => {
-        this.array.push(this.createEntity(object));
-      });
+    static create(realmCollection, entityClass) {
+        return new Proxy(new RealmResultsProxy(realmCollection, entityClass), RealmResultsProxyHandler);
     }
-  }
 
-  filter(predicate, thisArg) {
-    this.materialiseArray();
-    return this.array.filter(predicate, thisArg);
-  }
+    constructor(realmCollection, entityClass) {
+        this.entityClass = entityClass;
+        this.realmCollection = realmCollection;
+        this.array = [];
+    }
 
-  /*
-  Uses underlying collection. The objects would not have access to business methods on avni model
-   */
-  filterInternal(predicate, thisArg) {
-    return this.realmCollection.filter(predicate, thisArg);
-  }
+    setLogQueries(value) {
+        this.logQueries = value;
+    }
 
-  filtered(query, ...args) {
-    if (this.logQueries)
-      console.log(this.entityClass, query, ...args);
-    return RealmResultsProxy.create(this.realmCollection.filtered(query, ...args), this.entityClass);
-  }
+    createEntity(object) {
+        return new this.entityClass(object);
+    }
 
-  getAt(index) {
-    if (this.realmCollection.length <= index) return null;
+    asArray() {
+        this.materialiseArray();
+        return this.array;
+    }
 
-    const realmCollectionElement = this.realmCollection[index];
-    if (_.isNil(realmCollectionElement)) return null;
-    return this.createEntity(realmCollectionElement);
-  }
+    materialiseArray() {
+        if (this.array.length !== this.realmCollection.length) {
+            this.realmCollection.forEach((object, index) => {
+                this.array.push(this.createEntity(object));
+            });
+        }
+    }
 
-  forEach(callback, thisArg) {
-    return this.realmCollection.forEach((object, index) => {
-      return callback(this.createEntity(object), index, this);
-    }, thisArg);
-  }
+    filter(predicate, thisArg) {
+        this.materialiseArray();
+        return this.array.filter(predicate, thisArg);
+    }
 
-  isEmpty() {
-    return this.realmCollection.isEmpty();
-  }
+    /*
+    Uses underlying collection. The objects would not have access to business methods on avni model
+     */
+    filterInternal(predicate, thisArg) {
+        return this.realmCollection.filter(predicate, thisArg);
+    }
 
-  join(separator) {
-    return this.realmCollection.join(separator);
-  }
+    filtered(query, ...args) {
+        if (this.logQueries)
+            console.log(this.entityClass, query, ...args);
+        const realmResultsProxy = RealmResultsProxy.create(this.realmCollection.filtered(query, ...args), this.entityClass);
+        realmResultsProxy.setLogQueries(this.logQueries);
+        return realmResultsProxy;
+    }
 
-  map(callback, thisArg) {
-    return this.realmCollection.map((object, index) => {
-      return callback(this.createEntity(object), index, this);
-    }, thisArg);
-  }
+    getAt(index) {
+        if (this.realmCollection.length <= index) return null;
 
-  /*
-  Uses underlying collection. The objects would not have access to business methods on avni model
-   */
-  mapInternal(callback, thisArg) {
-    return this.realmCollection.map((object, index) => {
-      return callback(object, index, this);
-    }, thisArg);
-  }
+        const realmCollectionElement = this.realmCollection[index];
+        if (_.isNil(realmCollectionElement)) return null;
+        return this.createEntity(realmCollectionElement);
+    }
 
-  max(property) {
-    return this.realmCollection.max(property);
-  }
+    forEach(callback, thisArg) {
+        return this.realmCollection.forEach((object, index) => {
+            return callback(this.createEntity(object), index, this);
+        }, thisArg);
+    }
 
-  min(property) {
-    return this.realmCollection.min(property);
-  }
+    isEmpty() {
+        return this.realmCollection.isEmpty();
+    }
 
-  slice(start, end) {
-    return this.realmCollection.slice(start, end).map((x) => this.createEntity(x));
-  }
+    join(separator) {
+        return this.realmCollection.join(separator);
+    }
 
-  some(callback, thisArg) {
-    return this.realmCollection.some((object, index, collection) => {
-      return callback(this.createEntity(object), index, this);
-    }, thisArg);
-  }
+    map(callback, thisArg) {
+        return this.realmCollection.map((object, index) => {
+            return callback(this.createEntity(object), index, this);
+        }, thisArg);
+    }
 
-  sorted(descriptor, reverse) {
-    const realmCollection = _.isNil(reverse) ? this.realmCollection.sorted(descriptor) : this.realmCollection.sorted(descriptor, reverse);
-    return RealmResultsProxy.create(realmCollection, this.entityClass);
-  }
+    /*
+    Uses underlying collection. The objects would not have access to business methods on avni model
+     */
+    mapInternal(callback, thisArg) {
+        return this.realmCollection.map((object, index) => {
+            return callback(object, index, this);
+        }, thisArg);
+    }
 
-  sum(property) {
-    return this.realmCollection.sum(property);
-  }
+    max(property) {
+        return this.realmCollection.max(property);
+    }
 
-  getLength() {
-    return this.realmCollection.length;
-  }
+    min(property) {
+        return this.realmCollection.min(property);
+    }
 
-  get optional() {
-    return this.realmCollection.optional;
-  }
+    slice(start, end) {
+        return this.realmCollection.slice(start, end).map((x) => this.createEntity(x));
+    }
 
-  get type() {
-    return this.realmCollection.type;
-  }
+    some(callback, thisArg) {
+        return this.realmCollection.some((object, index, collection) => {
+            return callback(this.createEntity(object), index, this);
+        }, thisArg);
+    }
 
-  [Symbol.iterator]() {
-    return this.realmCollection.map((x) => new this.entityClass(x))[Symbol.iterator]();
-  }
+    sorted(descriptor, reverse) {
+        const realmCollection = _.isNil(reverse) ? this.realmCollection.sorted(descriptor) : this.realmCollection.sorted(descriptor, reverse);
+        return RealmResultsProxy.create(realmCollection, this.entityClass);
+    }
 
-  find(filterCallback, thisArg) {
-    const findFunc = (object, index, collection) => {
-      return filterCallback(this.createEntity(object), index, this);
-    };
-    return _.isNil(thisArg) ? this.realmCollection.find(findFunc) : this.realmCollection.find(findFunc, thisArg);
-  }
+    sum(property) {
+        return this.realmCollection.sum(property);
+    }
+
+    getLength() {
+        return this.realmCollection.length;
+    }
+
+    get optional() {
+        return this.realmCollection.optional;
+    }
+
+    get type() {
+        return this.realmCollection.type;
+    }
+
+    [Symbol.iterator]() {
+        return this.realmCollection.map((x) => new this.entityClass(x))[Symbol.iterator]();
+    }
+
+    find(filterCallback, thisArg) {
+        const findFunc = (object, index, collection) => {
+            return filterCallback(this.createEntity(object), index, this);
+        };
+        return _.isNil(thisArg) ? this.realmCollection.find(findFunc) : this.realmCollection.find(findFunc, thisArg);
+    }
 }
 
 export default RealmResultsProxy;
