2618618b93953d6c3aa62ccdafab37a2af585976
Vinay Venu <ijkvinay@gmail.com>
2023-12-21 08:58:04 +0530

    avniproject/avni-client#1237 | Add tests to verify age

--- test/util/AgeUtilTest.js
+++ test/util/AgeUtilTest.js
@@ -1,6 +1,7 @@
 import {assert} from "chai";
 import _ from "lodash";
 import AgeUtil from "../../src/utility/AgeUtil";
+import moment from "moment";
 
 describe('AgeUtilTest', () => {
   beforeEach(() => {
@@ -29,5 +30,22 @@ describe('AgeUtilTest', () => {
     const i18n = {t: _.identity};
     const dateOfBirth = '2015-07-10';
     assert.equal(AgeUtil.getDisplayAge(dateOfBirth, i18n), "7 years");
+  });
+
+  describe("ageInYears", () => {
+    const age = (dob) => {
+      return AgeUtil.getAgeInYears(dob, moment("2020-01-01"))
+    }
+
+    it("should be calculated correctly", () => {
+      assert.equal(age('2019-01-01'), 1);
+      assert.equal(age('2019-01-02'), 0);
+      assert.equal(age('2018-12-31'), 1);
+
+      assert.equal(age('2000-01-01'), 20);
+      assert.equal(age('2020-01-01'), 0);
+
+      assert.equal(age('2021-01-01'), -1);
+    })
   });
 });
