98afbda7ab4a3c4ee772bb00de14a13b4b32feaf
Joy A <itsjoy@gmail.com>
2024-09-05 11:16:17 +0530

    avniproject/avni-server#781 | Add type to StandardReportCardType

--- test/ReportCardTest.js
+++ test/ReportCardTest.js
@@ -4,9 +4,9 @@ import StandardReportCardType from "../src/StandardReportCardType";
 import {assert} from 'chai';
 
 it('is subject type filter supported', function () {
-    const approvedType = StandardReportCardTypeFactory.create({name: StandardReportCardType.type.Approved});
+    const approvedType = StandardReportCardTypeFactory.create({name: 'Approved', type: StandardReportCardType.types.Approved});
     const reportCard = ReportCardFactory.create({standardReportCardType: approvedType});
     assert.equal(reportCard.isSubjectTypeFilterSupported(), false);
-    reportCard.standardReportCardType = StandardReportCardTypeFactory.create({name: StandardReportCardType.type.ScheduledVisits});
+    reportCard.standardReportCardType = StandardReportCardTypeFactory.create({name: 'Scheduled visits', type: StandardReportCardType.types.ScheduledVisits});
     assert.equal(reportCard.isSubjectTypeFilterSupported(), true);
 });
